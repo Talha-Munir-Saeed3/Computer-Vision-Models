1d8b94992563d930d6be4db913d48c9346d0c0c5
Talha-Munir-Saeed3 <munirtalha613@gmail.com>
2025-07-30 14:16:55 +0500

    Understanding Color Spaces in OpenCV

---
+++ Day-1/OpenCV/Lesson-4/Colorspaces/main.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+
+img=cv2.imread(r'C:\Users\Talha\Desktop\CODES\DLP\Computer-Vision-Models\Day-1\OpenCV\Lesson-4\Colorspaces\Data\scene.png')
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+
+
